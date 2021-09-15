138fa5894ac9e581e2ed4287f9f239a10eecb670
hey-shashikant <shashikantsolanki041@gmail.com>
2021-09-15 14:16:08 +0530

    Learn Python Day 1 (Set and Tuples)

---
+++ #6.py
@@ -0,0 +1,14 @@
+#round bracket defines it's tuple
+#tuples are immutable
+#when you dont want the values to change use tuples
+tup = (1,2,3,4)
+print(tup)
+#tup[1] = 2
+print(tup)
+#set is stored in curly bracked
+#set uses the concept of hash function
+s = {10,2,9,1,2,3,4,5,54,1,23,45,67,8,9,766}
+print(s)
+
+s.sort()
+print(s)
