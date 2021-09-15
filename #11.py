e7917d73046ec353624443ee022a2318d4169c16
hey-shashikant <shashikantsolanki041@gmail.com>
2021-09-15 22:13:55 +0530

    Operators in Python

---
+++ #11.py
@@ -0,0 +1,28 @@
+#operators in python
+x = 2
+y = 3
+
+
+x+=2
+print(x)
+
+a,b = 10,20
+print(a)
+print(b)
+
+#unary operator
+
+n = 5;
+n+=1
+print(n)
+
+#relational operators
+
+print(a < b)
+print(a > b)
+print(a == b)
+
+x = False
+if(not x):
+	print("shashikant")
+
